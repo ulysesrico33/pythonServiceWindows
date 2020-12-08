a8c5a8a8d6eced60c387912a3332a2a5fffee81a
Ulyses <ulyses.rico.2012@gmail.com>
2020-12-07 22:45:08 -0600

    ...

---
+++ svcmain.py
@@ -0,0 +1,26 @@
+import time
+import random
+from pathlib import Path
+from SMWinservice import SMWinservice
+
+class svcmain(SMWinservice):
+    _svc_name_ = "PythonCornerExample"
+    _svc_display_name_ = "Python Corner's Winservice Example"
+    _svc_description_ = "That's a great winservice! :)"
+
+    def start(self):
+        self.isrunning = True
+
+    def stop(self):
+        self.isrunning = False
+
+    def main(self):
+        i = 0
+        while self.isrunning:
+            random.seed()
+            x = random.randint(1, 1000000)
+            Path(f'c:{x}.txt').touch()
+            time.sleep(5)
+
+if __name__ == '__main__':
+    svcmain.parse_command_line()

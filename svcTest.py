ac9033ffde45f6c205ae59e0e133c5ed6412cd50
Ulyses <ulyses.rico.2012@gmail.com>
2020-12-16 12:26:14 -0600

    ...

---
+++ svcTest.py
@@ -0,0 +1,26 @@
+import time
+import random
+from pathlib import Path
+from SMWinservice import SMWinservice
+import os
+
+class svcTest(SMWinservice):
+    _svc_name_ = "svcTEST"
+    _svc_display_name_ = "svcTEST"
+    _svc_description_ = "Let's see if this works..."
+
+    def start(self):
+        self.isrunning = True
+
+    def stop(self):
+        self.isrunning = False
+
+    def main(self):
+        while self.isrunning:
+            os.system('notepad')
+            
+            
+
+
+if __name__ == '__main__':
+    svcTest.parse_command_line()
